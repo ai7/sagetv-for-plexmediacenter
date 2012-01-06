d48ec077df3b85f97d845ac5015c24eebd18f2b6
Mreid910@gmail.com <Mreid910@gmail.com@7d984ce1-2ca6-e77c-0e17-e972783e0807>
2012-01-06 02:10:14 +0000

    Code clean up, optimization.

--- branches/SageTVScanner/Scanners/Series/SageTV Scanner.py
+++ branches/SageTVScanner/Scanners/Series/SageTV Scanner.py
@@ -1,62 +1,73 @@
+#    Author: PiX(64) - Reid, Michael
+# 
+#    Source: This scanner was built using code found in Plex Media Scanner.py.
+#    Thank you to the plex development team for their help and great starting point.
 #
-# PiX64 - initial code
-#
+#    Purpose:  The purpose of SageTV Scanner.py is to provide a means for Plex Media Center to scan
+#    SageTV Recording directories.  This scanner will find ALL sage recordings including Movies and TV
+#    This file used in conjuction with SageTV BMT Scanner (BMTAgent.bundle) will allow users to include
+#    sage tv recordings and all BMT scraped metadata into their plex media server setup.
+#-----------------------------------------
 import re, os, os.path
 import Media, VideoFiles, Stack, Utils
 from mp4file import mp4file, atomsearch
 
+#  
+#  regular expression used to find sagetv recordings in the specified directory
+#  SageTV files are formated as show-episode-randomnumbers.txt and can in some cases include SxxExx
+#  information.
+#-------------------
 sage_regex = '(?P<show>.*?)-(?P<episode>.*?)-'
-episode_regexps = [
-    '(?P<show>.*?)-(?P<episode>.*?)' # Matcher for Sage Recordings (Movie and TV)
-  ]
-    #'(?P<show>.*?)[sS](?P<season>[0-9]+)[\._ ]*[eE](?P<ep>[0-9]+)([- ]?[Ee+](?P<secondEp>[0-9]+))?',                           # S03E04-E05
-    #'(?P<show>.*?)[sS](?P<season>[0-9]{2})[\._\- ]+(?P<ep>[0-9]+)',                                                            # S03-03
-    #'(?P<show>.*?)([^0-9]|^)(?P<season>[0-9]{1,2})[Xx](?P<ep>[0-9]+)(-[0-9]+[Xx](?P<secondEp>[0-9]+))?',                       # 3x03
-    #'(.*?)[^0-9a-z](?P<season>[0-9]{1,2})(?P<ep>[0-9]{2})([\.\-][0-9]+(?P<secondEp>[0-9]{2})([ \-_\.]|$)[\.\-]?)?([^0-9a-z%]|$)' # .602.    
-#  ]
 
 # Look for episodes.
 def Scan(path, files, mediaList, subdirs):
-  #print "MREID -- Entering SCAN!"
+
   # Scan for video files.
   VideoFiles.Scan(path, files, mediaList, subdirs)
   
-  # Take top two as show/season, but require at least the top one.
   paths = Utils.SplitPath(path)
-  #print "MREID - files  = %s" % files
-  #print "MREID - paths = %s" % len(paths)
-  #print "MREID - paths[0] = %s" % len(paths[0])
-
-  # Run the select regexps we allow at the top level.
-  ep_num = 0
+  
+  # default episode number to 0.  We wont' grab episode number or season number in this scanner
+  # that info will be obtained in SageTVBMTAgent.  In order to send a valid object we must default
+  # season and episode to 0,0.  We are setting episode number outside of the loop used to interate over
+  # files in order to increment the episodes scanned from 1 - x
+  #--------------
+  ep_num = 0 
+  
+  # loop used to interate over all files found in VideoFiles.Scan above
   for i in files:
     file = os.path.basename(i)
-    #print "MREID - file = %s" % file
-    
-#    for rx in episode_regexps:
-      #print "MREID - inside 2nd for loop"
-      #print "MREID - rx = %s" % rx
     match = re.search(sage_regex, file, re.IGNORECASE)
-      #print "MREID - match = %s" % match
+    #print "File name = %s" % file
+    
+    # If we find a match using the regex above, extract data, create media object, and append to results
     if match:
-        # Extract data.
+      # Extract data.
       show = match.group('show')
       s_num = 0
       ep_num = ep_num + 1
       ep_name = match.group('episode')
-      print "Show = %s" % show
-      print "Episode = %s" % ep_name
+      
+      #print "Show = %s" % show
+      #print "Episode = %s" % ep_name
+      
       tv_show = Media.Episode(show,s_num, ep_num,ep_name, '')
-      print "MREID - TVShow = %s" % tv_show
+      
+      #print "MREID - TVShow = %s" % tv_show
+      
       tv_show.display_offset = 0
       tv_show.parts.append(i)
       mediaList.append(tv_show)
     else:
-      print "MREID - NO MATCH!"
+      print "**** No Match found for file %s" % file
           
   # Stack the results.
   Stack.Scan(path, files, mediaList, subdirs)
   
+#
+#    The following code was present in the original code Plex Media Scanner.py
+#    It was left as is as I am not 100% sure of its use.
+#----------------------------
 def find_data(atom, name):
   child = atomsearch.find_path(atom, name)
   data_atom = child.find('data')
