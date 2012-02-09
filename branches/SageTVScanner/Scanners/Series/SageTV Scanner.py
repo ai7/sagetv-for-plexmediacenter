c3a1df915386a1040071fb6f79eb15b964e6f6d3
Mreid910@gmail.com <Mreid910@gmail.com@7d984ce1-2ca6-e77c-0e17-e972783e0807>
2012-02-09 23:25:06 +0000

    Final update to originalairdate.

--- branches/SageTVScanner/Scanners/Series/SageTV Scanner.py
+++ branches/SageTVScanner/Scanners/Series/SageTV Scanner.py
@@ -135,7 +135,7 @@ def Scan(path, files, mediaList, subdirs):
           if(category.find("Movie")<0 and category.find("Movies")<0 and category.find("Film")<0):
             if (s_num == "0"):
               startTime = float(showMF.get('OriginalAiringDate') // 1000)
-              if (startTime):
+              if (startTime > 0):
                 airDate = date.fromtimestamp(startTime)
                 print "****** S_num isnt set and = %s. using showyear which = %s" % (s_num,airDate.year)
                 tv_show = Media.Episode(showTitle, airDate.year, None, episodeTitle, None)
